ee1439bf279e738f90d1a403c294ad1cda495fcd
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2024-04-11 13:21:44 -0300

    Consertando o nome da tabela pivot da relação de Categoria e Produto

--- database/migrations/1712841380274create_categoria_produto_table.ts
+++ database/migrations/1712841380274create_categoria_produto_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'create_categoria_produtos'
+  protected tableName = 'categoria_produto'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
