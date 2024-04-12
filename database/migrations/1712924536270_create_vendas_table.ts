5eb338cbf76fa2ba90332891a432820742dcbcf9
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2024-04-12 10:16:41 -0300

    Consertando um erro da migration de Vendas

--- database/migrations/1712924536270_create_vendas_table.ts
+++ database/migrations/1712924536270_create_vendas_table.ts
@@ -6,8 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('cliente_id').references('id').inTable('clientes')
-      table.integer('produto_id').references('id').inTable('produtos')
+      table.integer('cliente_id').unsigned()
+      table.integer('produto_id').unsigned()
+      table.foreign('cliente_id').references('id').inTable('clientes').onDelete('cascade')
+      table.foreign('produto_id').references('id').inTable('produtos').onDelete('cascade')
       table.integer('quantidade')
       table.float('preco_unitario')
       table.float('preco_total')
